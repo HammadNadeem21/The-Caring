44752171f760195fb804de3dbc6d13e22fb18dd7
HammadNadeem21 <nhammad424@gmail.com>
2026-05-02 22:15:02 +0500

    updated

--- contact-form.php
+++ contact-form.php
@@ -1,45 +1,64 @@
 <?php
-$name = $_POST['fname'];
-$emailHelp = $_POST['email'];
-$lastName = $_POST['lname'];
-$phone = $_POST['phone'];
-$comments = $_POST['message'];
+// Include PHPMailer classes from Composer
+require 'vendor/autoload.php';
 
-if (!empty($name) && !empty($phone) && !empty($emailHelp)) {
-	global $to_email, $vpb_message_body, $headers;
-	$to_email = "info@thecaring.co.uk";
-	$email_subject = "New Inquiry from " . $name . " " . $lastName;
-	$vpb_message_body = nl2br("Dear Admin,\n
-	The user whose detail is shown below has sent this message from " . $_SERVER['HTTP_HOST'] . " dated " . date('d-m-Y') . ".\n
-	
-	Name: " . $name . " " . $lastName . "\n
-	Email Address: " . $emailHelp . "\n
-    Phone: " . $phone . "\n
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-	Message: " . $comments . "\n
-	User Ip:" . getHostByName(getHostName()) . "\n
-	Thank You!\n\n");
+// Sanitize and collect input data
+$name = strip_tags(trim($_POST['fname'] ?? ''));
+$lastName = strip_tags(trim($_POST['lname'] ?? ''));
+$emailHelp = filter_var(trim($_POST['email'] ?? ''), FILTER_SANITIZE_EMAIL);
+$phone = strip_tags(trim($_POST['phone'] ?? ''));
+$comments = strip_tags(trim($_POST['message'] ?? ''));
 
-	//Set up the email headers
-	$headers = "From: $name <$emailHelp>\r\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	$headers .= "Message-ID: <" . time() . rand(1, 1000) . "@" . $_SERVER['SERVER_NAME'] . ">" . "\r\n";
+// Basic validation
+if (!empty($name) && !empty($phone) && filter_var($emailHelp, FILTER_VALIDATE_EMAIL)) {
 
-	if (@mail($to_email, $email_subject, $vpb_message_body, $headers)) {
+	$mail = new PHPMailer(true);
+
+	try {
+		// --- Real Production Settings ---
+		$mail->isSMTP();
+		$mail->Host       = 'mail.thecaring.co.uk';
+		$mail->SMTPAuth   = true;
+		$mail->Username   = 'info@thecaring.co.uk';
+		$mail->Password   = 'CLIENTS_REAL_PASSWORD'; // Client must enter their password here
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // Use SSL
+		$mail->Port       = 465;
+
+		// --- Recipients ---
+		$mail->setFrom('noreply@thecaring.co.uk', $name);
+		$mail->addAddress('info@thecaring.co.uk', 'Admin');
+		$mail->addReplyTo($emailHelp, $name);
+
+		// --- Content ---
+		$mail->isHTML(true);
+		$mail->Subject = "New Inquiry from " . $name . " " . $lastName;
+
+		$message_body = "Dear Admin,<br><br>";
+		$message_body .= "The user whose detail is shown below has sent this message from " . htmlspecialchars($_SERVER['HTTP_HOST']) . ".<br><br>";
+		$message_body .= "<b>Name:</b> " . htmlspecialchars($name . " " . $lastName) . "<br>";
+		$message_body .= "<b>Email Address:</b> " . htmlspecialchars($emailHelp) . "<br>";
+		$message_body .= "<b>Phone:</b> " . htmlspecialchars($phone) . "<br><br>";
+		$message_body .= "<b>Message:</b><br>" . nl2br(htmlspecialchars($comments)) . "<br><br>";
+		$message_body .= "Thank You!";
+
+		$mail->Body = $message_body;
+
+		$mail->send();
 		$status = 'Success';
-		//Displays the success message when email message is sent
-		$output = "Congrats " . $name . ", your email message has been sent successfully! We will get back to you as soon as possible. Thanks.";
-	} else {
+		$output = "Congrats " . htmlspecialchars($name) . ", your message has been sent to our testing inbox!";
+
+	} catch (Exception $e) {
 		$status = 'error';
-		//Displays an error message when email sending fails
-		$output = "Sorry, your email could not be sent at the moment. Please try again or contact this website admin to report this error message if the problem persist. Thanks.";
+		$output = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 	}
 
 } else {
 	$status = 'error';
-	$output = "Please fill in all required fields (Name, Email, and Phone).";
+	$output = "Please fill in all required fields correctly (Valid Name, Email, and Phone).";
 }
+
 echo json_encode(array('status' => $status, 'msg' => $output));
-
-
 ?>
